703edd1d303be1165de0bbcd6d7e151cbb949032
Ilhan Basic <ilhanbasic456@gmail.com>
2024-12-12 19:17:05 +0100

    added search and filters in admin page for better searching

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -4,22 +4,131 @@ import { AuthContext } from "../context/AuthContext";
 import Modal from "../components/Modal.jsx";
 import { getTranslation } from "../data/translation.js";
 import API_URL from "../API_URL.js";
+function search(data, searchValue) {
+  return data.filter((item) => {
+    return Object.values(item).some((value) =>
+      value.toString().toLowerCase().includes(searchValue.toLowerCase())
+    );
+  });
+}
+
 function Section({ title, data, onEdit, onDelete, onCancel }) {
+  const [filteredData, setFilteredData] = useState(data);
+
+  useEffect(() => {
+    setFilteredData(data);
+  }, [data]);
+
+  function handleSubmitSearch(e) {
+    e.preventDefault();
+    const searchValue = e.target[0].value.trim();
+    e.target[0].value = "";
+
+    const results = search(data, searchValue);
+    setFilteredData(results);
+  }
+  function filterVehiclesAvailability(vehicles, status) {
+    if (status === "Sve") return vehicles;
+    return vehicles.filter((vehicle) => vehicle.status === status);
+  }
+  function filterReservationsAvailability(reservations, status) {
+    if (status === "Sve") return reservations;
+    return reservations.filter(
+      (reservation) => reservation.reservationStatus === status
+    );
+  }
+  const detailsFields = ["registrationNumber","picture","fuelType","numOfDoors","transmission","creditCardNumber","firstName","lastName","phoneNumber","email","latitude","longitude"];
+  function handleShowDetails(){
+    
+  }
+
   return (
-    <div>
+    <>
       <h1>{title}</h1>
       {title !== "Rezervacije" && (
         <button onClick={() => onEdit(null)}>Dodaj novi red</button>
       )}
-      <div>
-        {data && data.length > 0 ? (
-          data.map((item) => (
+
+      <form className="search-form" onSubmit={handleSubmitSearch}>
+        <input className="search-input" type="text" placeholder="Pretraga" />
+        <button className="search-button">Pretraži</button>
+      </form>
+      {
+        <div className="results-count">
+          <p>Ukupno pronađeno: {filteredData.length}</p>
+        </div>
+      }
+      {title === "Vozila" && (
+        <button
+          className="filter-button"
+          onClick={() =>
+            setFilteredData(filterVehiclesAvailability(data, "Dostupno"))
+          }
+        >
+          Prikaži samo dostupna vozila
+        </button>
+      )}
+      {title === "Vozila" && (
+        <button
+          className="filter-button"
+          onClick={() =>
+            setFilteredData(filterVehiclesAvailability(data, "Zauzeto"))
+          }
+        >
+          Prikaži samo zauzeta vozila
+        </button>
+      )}
+      {title === "Vozila" && (
+        <button
+          className="filter-button"
+          onClick={() =>
+            setFilteredData(filterVehiclesAvailability(data, "Sve"))
+          }
+        >
+          Prikaži sva vozila
+        </button>
+      )}
+      {title === "Rezervacije" && (
+        <button
+          className="filter-button"
+          onClick={() =>
+            setFilteredData(filterReservationsAvailability(data, "Aktivna"))
+          }
+        >
+          Prikaži samo aktivne rezervacije
+        </button>
+      )}
+      {title === "Rezervacije" && (
+        <button
+          className="filter-button"
+          onClick={() =>
+            setFilteredData(filterReservationsAvailability(data, "Otkazana"))
+          }
+        >
+          Prikaži samo otkazane rezervacije
+        </button>
+      )}
+      {title === "Rezervacije" && (
+        <button
+          className="filter-button"
+          onClick={() =>
+            setFilteredData(filterReservationsAvailability(data, "Sve"))
+          }
+        >
+          Prikaži sve rezervacije
+        </button>
+      )}
+      <>
+        {filteredData && filteredData.length > 0 ? (
+          filteredData.map((item) => (
             <div key={item.id} className="table-row">
               <div className="vehicle-info">
                 {Object.entries(item)
                   .filter(
                     ([key, value]) =>
-                      typeof value !== "object" || value === null
+                      typeof value !== "object" ||
+                      value === null ||
+                      key === "id"
                   )
                   .map(([key, value], index) => (
                     <div key={index} className="vertical-table-row">
@@ -27,11 +136,12 @@ function Section({ title, data, onEdit, onDelete, onCancel }) {
                       <span>{value}</span>
                     </div>
                   ))}
+                  <button onClick={handleShowDetails}>Detalji</button>
               </div>
               {item.picture && (
                 <div className="vehicle-image-container">
                   <img
-                    src={item.picture || "/path/to/placeholder-image.jpg"} // Fallback to placeholder if no image
+                    src={item.picture}
                     alt="Vehicle"
                     className="vehicle-image"
                   />
@@ -57,10 +167,11 @@ function Section({ title, data, onEdit, onDelete, onCancel }) {
         ) : (
           <p>Nema podataka</p>
         )}
-      </div>
-    </div>
+      </>
+    </>
   );
 }
+
 export default function AdminPage() {
   const { token, isAdmin } = useContext(AuthContext);
   const navigate = useNavigate();

4dae91508a5837cb548877b327e1eecf391669d7
Ilhan Basic <ilhanbasic456@gmail.com>
2025-01-13 16:51:54 +0100

    changed api_url

--- rentora-frontend/src/API_URL.js
+++ rentora-frontend/src/API_URL.js
@@ -1,3 +1,3 @@
-// const API_URL = "https://rentora-czb7hwc5dxhxhxd5.germanywestcentral-01.azurewebsites.net//api";
-const API_URL = "https://localhost:7247/api";
+const API_URL = "https://rentora-czb7hwc5dxhxhxd5.germanywestcentral-01.azurewebsites.net//api";
+// const API_URL = "https://localhost:7247/api";
 export default API_URL;
